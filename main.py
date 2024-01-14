0bb814d6a43faeb11894a381e45c85e2905107ba
Ahmad <kamenrider503@gmail.com>
2024-01-14 23:27:01 +0800

    Semua sekali dh up-to-date ikut render-template python, and dh functional seperti biasa.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask
 from flask.templating import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def main():
     return render_template("Landing_Page.html")
+
+
+@app.route("/Syafiq")
+def Syafiq_Page():
+    return render_template("Syafiq-index.html")
+
+
+@app.route("/Phong")
+def Phong_Page():
+    return render_template("Phiraphong-index.html")
+
+
+@app.route("/Ahmad")
+def Ahmad_Page():
+    return render_template("Ahmad-index.html")
+
 
 if __name__ == '__main__':
     app.run()
